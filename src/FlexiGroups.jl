c03df9c175d9a3e20e9eef4ab0be1a4b62cf5064
Alexander Plavin <alexander@plav.in>
2023-01-01 00:00:00 +0000

    split group() into separate package

--- src/FlexiGroups.jl
+++ src/FlexiGroups.jl
@@ -1,3 +1,161 @@
 module FlexiGroups
 
+using Dictionaries
+
+export group, groupview, groupfind, groupmap
+
+
+function groupfind(f, X; kwargs...)
+    (; dct, starts, rperm) = _group_core(f, X, keys(X); kwargs...)
+    mapvalues(dct) do gid
+        @view rperm[starts[gid + 1]:-1:1 + starts[gid]]
+    end
+end
+
+function groupview(f, X; kwargs...)
+    (; dct, starts, rperm) = _group_core(f, X, keys(X); kwargs...)
+    mapvalues(dct) do gid
+        ix = @view rperm[starts[gid + 1]:-1:1 + starts[gid]]
+        @view X[ix]
+    end
+end
+
+function group(f, X; kwargs...)
+    (; dct, starts, rperm) = _group_core(f, X, values(X); kwargs...)
+    mapvalues(dct) do gid
+        @view rperm[starts[gid + 1]:-1:1 + starts[gid]]
+    end
+end
+
+function groupmap(f, ::typeof(length), X; kwargs...)
+    (; dct, starts, rperm) = _group_core(f, X, similar(X, Nothing); kwargs...)
+    mapvalues(dct) do gid
+        starts[gid + 1] - starts[gid]
+    end
+end
+
+function groupmap(f, ::typeof(first), X; kwargs...)
+    (; dct, starts, rperm) = _group_core(f, X, keys(X); kwargs...)
+    mapvalues(dct) do gid
+        ix = rperm[starts[gid + 1]]
+        X[ix]
+    end
+end
+
+function groupmap(f, ::typeof(last), X; kwargs...)
+    (; dct, starts, rperm) = _group_core(f, X, keys(X); kwargs...)
+    mapvalues(dct) do gid
+        ix = rperm[1 + starts[gid]]
+        X[ix]
+    end
+end
+
+function groupmap(f, ::typeof(only), X; kwargs...)
+    (; dct, starts, rperm) = _group_core(f, X, keys(X); kwargs...)
+    mapvalues(dct) do gid
+        starts[gid + 1] == starts[gid] + 1 || throw(ArgumentError("groupmap(only, X) requires that each group has exactly one element"))
+        ix = rperm[starts[gid + 1]]
+        X[ix]
+    end
+end
+
+_group_core(f, X, vals; dicttype=Dictionary) = _group_core(f, X, vals, dicttype)
+
+function _group_core(f, X::AbstractArray, vals::AbstractArray, ::Type{DT}) where {DT}
+    ngroups = 0
+    groups = similar(X, Int)
+    dct = DT{Core.Compiler.return_type(f, Tuple{_valtype(X)}), Int}()
+    @inbounds for (i, x) in pairs(X)
+        groups[i] = gid = get!(dct, f(x), ngroups + 1)
+        if gid == ngroups + 1
+            ngroups += 1
+        end
+    end
+
+    starts = zeros(Int, ngroups)
+    @inbounds for gid in groups
+        starts[gid] += 1
+    end
+    cumsum!(starts, starts)
+    push!(starts, length(groups))
+
+    rperm = similar(vals, Base.OneTo(length(vals)))
+    # rperm = Vector{_eltype(vals)}(undef, length(X))
+    @inbounds for (v, gid) in zip(vals, groups)
+        rperm[starts[gid]] = v
+        starts[gid] -= 1
+    end
+
+    # dct: key -> group_id
+    # rperm[starts[group_id + 1]:-1:1 + starts[group_id]] = group_values
+
+    return (; dct, starts, rperm)
+end
+
+
+function _group_core(f, X, vals, ::Type{DT}) where {DT}
+    ngroups = 0
+    groups = Int[]
+    dct = DT{Core.Compiler.return_type(f, Tuple{_valtype(X)}), Int}()
+    @inbounds for x in X
+        gid = get!(dct, f(x), ngroups + 1)
+        push!(groups, gid)
+        if gid == ngroups + 1
+            ngroups += 1
+        end
+    end
+
+    starts = zeros(Int, ngroups)
+    @inbounds for gid in groups
+        starts[gid] += 1
+    end
+    cumsum!(starts, starts)
+    push!(starts, length(groups))
+
+    rperm = Vector{_eltype(vals)}(undef, length(groups))
+    @inbounds for (v, gid) in zip(vals, groups)
+        rperm[starts[gid]] = v
+        starts[gid] -= 1
+    end
+
+    # dct: key -> group_id
+    # rperm[starts[group_id + 1]:-1:1 + starts[group_id]] = group_values
+
+    return (; dct, starts, rperm)
+end
+
+
+mapvalues(f, dict::AbstractDictionary) = map(f, dict)
+
+function mapvalues(f, dict::Dict{K, VI}) where {K, VI}
+    VO = Core.Compiler.return_type(f, Tuple{VI})
+    vals = dict.vals
+    newvals = similar(vals, VO)
+    @inbounds for i in dict.idxfloor:lastindex(vals)
+        if Base.isslotfilled(dict, i)
+            newvals[i] = f(vals[i])
+        end
+    end
+    _setproperties(dict, (;vals=newvals))
+end
+
+function _setproperties(d::Dict{K}, patch::NamedTuple{(:vals,), Tuple{AbstractVector{V}}}) where {K,V}
+    @assert length(d.keys) == length(patch.vals)
+    Dict{K,V}(copy(d.slots), copy(d.keys), patch.vals, d.ndel, d.count, d.age, d.idxfloor, d.maxprobe)
+end
+
+
+# make eltype tighter
+_eltype(::T) where {T} = _eltype(T)
+function _eltype(::Type{T}) where {T}
+    ETb = eltype(T)
+    ETb != Any && return ETb
+    # Base.eltype returns Any for mapped/flattened/... iterators
+    # here we attempt to infer a tighter type
+    ET = Core.Compiler.return_type(first, Tuple{T})
+    ET === Union{} ? Any : ET
+end
+
+_valtype(X) = _eltype(values(X))
+
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,142 @@ using TestItemRunner
 @run_package_tests
 
 
+@testitem "basic" begin
+    using Dictionaries
+
+    xs = 3 .* [1, 2, 3, 4, 5]
+    g = @inferred group(isodd, xs)
+    @test g == dictionary([true => [3, 9, 15], false => [6, 12]])
+    @test isconcretetype(eltype(g))
+    @test valtype(g) <: SubArray{Int}
+
+    # ensure we get a copy
+    xs[1] = 123
+    @test g == dictionary([true => [3, 9, 15], false => [6, 12]])
+
+
+    xs = 3 .* [1, 2, 3, 4, 5]
+    g = @inferred groupview(isodd, xs)
+    @test g == dictionary([true => [3, 9, 15], false => [6, 12]])
+    @test isconcretetype(eltype(g))
+    @test valtype(g) <: SubArray{Int}
+
+    # ensure we get a view
+    xs[1] = 123
+    @test g == dictionary([true => [123, 9, 15], false => [6, 12]])
+
+
+    xs = 3 .* [1, 2, 3, 4, 5]
+    g = @inferred groupfind(isodd, xs)
+    @test g == dictionary([true => [1, 3, 5], false => [2, 4]])
+    @test isconcretetype(eltype(g))
+    @test valtype(g) <: SubArray{Int}
+
+
+    g = @inferred(group(isnothing, [1, 2, 3, nothing, 4, 5, nothing]))
+    @test g == dictionary([false => [1, 2, 3, 4, 5], true => [nothing, nothing]])
+    @test valtype(g) <: SubArray{Union{Nothing, Int}}
+end
+
+@testitem "groupmap" begin
+    using Dictionaries
+
+    xs = 3 .* [1, 2, 3, 4, 5]
+    @test @inferred(groupmap(isodd, length, xs)) == dictionary([true => 3, false => 2])
+    @test @inferred(groupmap(isodd, first, xs)) == dictionary([true => 3, false => 6])
+    @test @inferred(groupmap(isodd, last, xs)) == dictionary([true => 15, false => 12])
+    @test_throws "exactly one element" groupmap(isodd, only, xs)
+    @test @inferred(groupmap(isodd, only, [10, 11])) == dictionary([false => 10, true => 11])
+end
+
+@testitem "iterators" begin
+    using Dictionaries
+
+    xs = (3x for x in [1, 2, 3, 4, 5])
+    g = @inferred group(isodd, xs)
+    @test g == dictionary([true => [3, 9, 15], false => [6, 12]])
+    @test isconcretetype(eltype(g))
+    @test valtype(g) <: SubArray{Int}
+end
+
+@testitem "dicttypes"
+    using Dictionaries
+
+    @testset for D in [Dict, Dictionary, UnorderedDictionary, ArrayDictionary]
+        @test group(isodd, 3 .* [1, 2, 3, 4, 5]; dicttype=D) |> pairs |> Dict == Dict(false => [6, 12], true => [3, 9, 15])
+        @test group(isodd, (3x for x in [1, 2, 3, 4, 5]); dicttype=D) |> pairs |> Dict == Dict(false => [6, 12], true => [3, 9, 15])
+    end
+end
+
+@testitem "structarray" begin
+    using Dictionaries
+    using StructArrays
+
+    xs = StructArray(a=3 .* [1, 2, 3, 4, 5])
+    g = @inferred group(x -> isodd(x.a), xs)
+    @test g == dictionary([true => [(a=3,), (a=9,), (a=15,)], false => [(a=6,), (a=12,)]])
+    @test isconcretetype(eltype(g))
+    @test g[false].a == [6, 12]
+
+    g = @inferred groupview(x -> isodd(x.a), xs)
+    @test g == dictionary([true => [(a=3,), (a=9,), (a=15,)], false => [(a=6,), (a=12,)]])
+    @test isconcretetype(eltype(g))
+    @test g[false].a == [6, 12]
+end
+
+@testitem "keyedarray" begin
+    using Dictionaries
+    using AxisKeys
+
+    xs = KeyedArray(1:5, a=3 .* [1, 2, 3, 4, 5])
+    g = @inferred group(isodd, xs)
+    @test g == dictionary([true => [1, 3, 5], false => [2, 4]])
+    @test isconcretetype(eltype(g))
+    @test_broken axiskeys(g[false]) == ([6, 12],)
+    @test_broken g[false](a=6) == 2
+
+    g = @inferred groupview(isodd, xs)
+    @test g == dictionary([true => [1, 3, 5], false => [2, 4]])
+    @test isconcretetype(eltype(g))
+    @test axiskeys(g[false]) == ([6, 12],)
+    @test g[false](a=6) == 2
+end
+
+@testitem "typedtable" begin
+    using Dictionaries
+    using TypedTables
+
+    xs = Table(a=3 .* [1, 2, 3, 4, 5])
+    g = @inferred group(x -> isodd(x.a), xs)
+    @test g == dictionary([true => [(a=3,), (a=9,), (a=15,)], false => [(a=6,), (a=12,)]])
+    @test isconcretetype(eltype(g))
+    @test g[false].a == [6, 12]
+
+    g = @inferred groupview(x -> isodd(x.a), xs)
+    @test g == dictionary([true => [(a=3,), (a=9,), (a=15,)], false => [(a=6,), (a=12,)]])
+    @test isconcretetype(eltype(g))
+    @test g[false].a == [6, 12]
+end
+
+@testitem "dictionary" begin
+    using Dictionaries
+    # view(dct, range) doesn't work for dictionaries by default
+    Base.view(d::AbstractDictionary, inds::AbstractArray) = Dictionaries.ViewArray{valtype(d), ndims(inds)}(d, inds)
+
+    xs = dictionary(3 .* [1, 2, 3, 4, 5] .=> 1:5)
+    g = @inferred group(isodd, xs)
+    @test g == dictionary([true => [1, 3, 5], false => [2, 4]])
+    @test isconcretetype(eltype(g))
+    @test g[false] == [2, 4]
+
+    g = @inferred groupview(isodd, xs)
+    @test g == dictionary([true => [1, 3, 5], false => [2, 4]])
+    @test isconcretetype(eltype(g))
+    @test g[false] == [2, 4]
+    xs[6] = 123
+    @test g == dictionary([true => [1, 3, 5], false => [123, 4]])
+end
+
 @testitem "_" begin
     import Aqua
     Aqua.test_all(FlexiGroups; ambiguities=false)
