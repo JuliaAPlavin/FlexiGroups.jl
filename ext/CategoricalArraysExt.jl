e873309950c711d9bf74b885a32f9718bc4b2d3b
Alexander Plavin <alexander@plav.in>
2026-03-01 00:00:00 +0000

    bump CategoricalArrays compat
    Co-Authored-By: Jakob Hoffmann <jfb-hoffmann@hotmail.de>

--- ext/CategoricalArraysExt.jl
+++ ext/CategoricalArraysExt.jl
@@ -2,7 +2,7 @@ module CategoricalArraysExt
 using CategoricalArrays
 import FlexiGroups: _possible_values
 
-_possible_values(X::CategoricalArray) = levels(X)
-_possible_values(X::AbstractArray{<:CategoricalValue}) = isempty(X) ? nothing : levels(first(X))
+_possible_values(X::CategoricalArray) = CategoricalArrays.unwrap.(levels(X))
+_possible_values(X::AbstractArray{<:CategoricalValue}) = isempty(X) ? nothing : CategoricalArrays.unwrap.(levels(first(X)))
 
 end
